dddef160ae7d08ec544d695483a6300b71bd8166
NITSAN <sanjay@nitsan.in>
2019-11-25 18:11:52 +0530

    [TODO] Release first public version

---
+++ Configuration/TypoScript/setup.ts
@@ -0,0 +1,11 @@
+# Includes EXT:Form Configuration
+<INCLUDE_TYPOSCRIPT: source="DIR:EXT:ns_theme_comingsoon/Configuration/TypoScript/Extensions/Form" extensions="ts">
+
+# Includes EXT:Fluid Configuration
+<INCLUDE_TYPOSCRIPT: source="DIR:EXT:ns_theme_comingsoon/Configuration/TypoScript/Extensions/Fluid" extensions="ts">
+
+# Includes Page Configuration
+<INCLUDE_TYPOSCRIPT: source="DIR:EXT:ns_theme_comingsoon/Configuration/TypoScript/Page" extensions="ts">
+
+# Includes EXT:Grids Configuration
+<INCLUDE_TYPOSCRIPT: source="DIR:EXT:ns_theme_comingsoon/Configuration/TypoScript/Extensions/GridElements" extensions="ts">
