ea300d3cc38a78f51adfca2c59d44d69d2ec50fb
NITSAN Technologies <sanjay@nitsan.in>
2020-04-17 17:01:14 +0530

    [ext_emconf] Update information

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,12 +15,11 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_email' => 'info@nitsan.in',
 	'author_company' => 'NITSAN Technologies Pvt Ltd',
 	'state' => 'stable',
-	'version' => '1.2.1',
+	'version' => '1.3.0',
 	'constraints' => array(
 		'depends' => array(
             'typo3' => '8.0.0-10.9.99',
-			'ns_basetheme' => '1.0.0-9.9.99',
-			'gridelements' => '1.0.0-10.99.99',
+			'ns_basetheme' => '1.0.0-10.9.99'
 		),
 		'conflicts' => array(
 		),
