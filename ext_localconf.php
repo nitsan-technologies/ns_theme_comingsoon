6f5ee83da67276a32dbdb7fc1a4870fbb8e4db1c
Mahesh Rathod <rmahesh.nitsan@gmail.com>
2021-08-05 20:07:25 +0530

    [TODO] Code improvements

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,7 +7,7 @@ if (!defined('TYPO3_MODE')) {
 // Let's configuration of this extension from "Extension Manager"
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY] = unserialize($_EXTCONF);
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ns_theme_comingsoon/Configuration/PageTSconfig/setup.ts">');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ns_theme_comingsoon/Configuration/PageTSconfig/setup.typoscript">');
 
 // Let's include PageTSconfig
 if (TYPO3_MODE === 'BE') {
