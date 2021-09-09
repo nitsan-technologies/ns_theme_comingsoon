c2410df08628eef17f7429a17db40ddffaff4be6
NITSAN Technologies <sanjay@nitsan.in>
2021-09-09 12:46:52 +0530

    [TODO] Author name change

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,11 +11,11 @@ $EM_CONF['ns_theme_comingsoon'] = array(
 	'title' => '[NITSAN] Coming Soon TYPO3 Template',
 	'description' => 'Maintenance mode TYPO3 template is carefully designed with attention to the details. Your visitors will love it. LIVE Demo: https://demo.t3terminal.com/?theme=t3t-coming-soon You can download PRO version with more-features & free-support at https://t3terminal.com/t3-coming-soon-maintenance-mode-typo3-template/',
 	'category' => 'templates',
-	'author' => 'T3:Ravi Gadhvi, QA:Vandna Kalivada',
+	'author' => 'NITSAN Technologies Pvt Ltd',
 	'author_email' => 'info@nitsan.in',
 	'author_company' => 'NITSAN Technologies Pvt Ltd',
 	'state' => 'stable',
-	'version' => '2.0.1',
+	'version' => '2.0.2',
 	'constraints' => array(
 		'depends' => array(
             'typo3' => '8.0.0-10.9.99',
