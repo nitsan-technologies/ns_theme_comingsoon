ab7569eaf6d5ed4eb3a4479c9a237a4c7378b243
NITSAN Technologies <sanjay@nitsan.in>
2022-01-03 14:51:57 +0530

    [TASK] Addev live demo page

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,15 @@
 <?php
-/*
- * This file is part of the package nitsan/ns-basetheme.
- *
- * For the full copyright and license information, please read the
- * LICENSE file that was distributed with this source code.
- */
 
 // Provide detailed information and depenencies of EXT:ns_theme_comingsoon
 $EM_CONF['ns_theme_comingsoon'] = array(
 	'title' => '[NITSAN] T3 Coming Soon | Maintenance Mode TYPO3 Template',
-	'description' => 'T3 Coming Soon: Maintenance Mode TYPO3 Template is carefully designed with attention to the details. This TYPO3 maintenance template is designed to catch attention and inform visitors what you and your TYPO3 website are up to.',
+	'description' => 'Maintenance Mode TYPO3 Template is carefully designed with attention to the details. This TYPO3 maintenance template is designed to catch attention and inform visitors what you and your TYPO3 website are up to. Live-Demo: https://demo.t3terminal.com/?theme=t3t-coming-soon Pro Support: https://t3terminal.com/t3-coming-soon-typo3-template',
 	'category' => 'templates',
-	'author' => 'NITSAN Technologies Pvt Ltd',
+	'author' => 'Team NITSAN',
 	'author_email' => 'info@nitsan.in',
 	'author_company' => 'NITSAN Technologies Pvt Ltd',
 	'state' => 'stable',
-	'version' => '12.0.0',
+	'version' => '3.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '10.0.0-11.5.99',
